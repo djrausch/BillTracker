c30db6d446e81d3a1bbbc759f6aad9dd75078051
DJ Rausch <djrausch3@gmail.com>
2016-05-30 15:25:20 -0700

    Upcomign bills

--- app/src/main/java/com/djrausch/billtracker/receivers/AlarmReceiver.java
+++ app/src/main/java/com/djrausch/billtracker/receivers/AlarmReceiver.java
@@ -8,6 +8,10 @@ import com.djrausch.billtracker.BillTrackerApplication;
 import com.djrausch.billtracker.models.Bill;
 import com.djrausch.billtracker.util.NotificationUtil;
 
+import org.joda.time.DateTime;
+
+import java.util.Date;
+
 import io.realm.RealmResults;
 
 /**
@@ -20,7 +24,9 @@ public class AlarmReceiver extends BroadcastReceiver {
         //Find upcoming bills to show notificaiton for
         //Bill b = BillTrackerApplication.getRealm().where(Bill.class).findFirst();
         RealmResults<Bill> bills = BillTrackerApplication.getRealm().where(Bill.class).findAll();
-        NotificationUtil.makeBillNotification(context, bills);
+        //Find bills in the next week.
+        RealmResults<Bill> bills1 = BillTrackerApplication.getRealm().where(Bill.class).between("dueDate", new Date(), new DateTime().plusWeeks(1).toDate()).findAll();
+        NotificationUtil.makeBillNotification(context, bills1);
 
     }
 }
