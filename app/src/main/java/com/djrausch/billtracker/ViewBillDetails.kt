b1f7018acc3a3b72ffff2ef5034fb0023ca7de1d
DJ Rausch <djrausch3@gmail.com>
2016-06-07 15:29:19 -0700

    Add View bill

---
+++ app/src/main/java/com/djrausch/billtracker/ViewBillDetails.kt
@@ -0,0 +1,12 @@
+package com.djrausch.billtracker
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewBillDetails : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_bill_details)
+    }
+}
