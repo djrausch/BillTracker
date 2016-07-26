f2ee5158ca789065ad701397b1580bc59f2ed889
DJ Rausch <djrausch3@gmail.com>
2016-07-25 23:47:48 -0700

    Model to java

---
+++ app/src/main/java/com/djrausch/billtracker/models/BillPaid.java
@@ -0,0 +1,33 @@
+package com.djrausch.billtracker.models;
+
+import java.util.Date;
+import java.util.UUID;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+/**
+ * Created by white on 7/25/2016.
+ */
+public class BillPaid extends RealmObject {
+    @PrimaryKey
+    public String uuid;
+    public Date date;
+
+    public BillPaid() {
+
+    }
+
+    public BillPaid(Date date) {
+        this.uuid = UUID.randomUUID().toString();
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "BillPaid{" +
+                "uuid='" + uuid + '\'' +
+                ", date=" + date +
+                '}';
+    }
+}
