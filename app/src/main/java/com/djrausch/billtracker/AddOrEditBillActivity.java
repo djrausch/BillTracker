76a360e9d93f799a67fb45ada546e140c42e8e99
DJ Rausch <djrausch3@gmail.com>
2016-07-26 17:21:28 -0700

    Add Or Edit to java

---
+++ app/src/main/java/com/djrausch/billtracker/AddOrEditBillActivity.java
@@ -0,0 +1,195 @@
+package com.djrausch.billtracker;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.djrausch.billtracker.adapters.RepeatingSpinnerAdapter;
+import com.djrausch.billtracker.models.Bill;
+import com.djrausch.billtracker.models.RepeatingItem;
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
+
+import org.joda.time.DateTime;
+
+import io.realm.Realm;
+
+public class AddOrEditBillActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+
+    private RepeatingItem repeatingItem;
+    private DateTime selectedDueDate;
+    private TextView dueDateSelect;
+    private Spinner repeatingSpinner;
+
+    private EditText name;
+    private EditText payUrl;
+
+
+    private boolean editing = false;
+    private Bill editBill;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_or_edit_bill);
+
+        dueDateSelect = (TextView) findViewById(R.id.due_date_select);
+        repeatingSpinner = (Spinner) findViewById(R.id.repeating_spinner);
+
+        name = (EditText) findViewById(R.id.name);
+        payUrl = (EditText) findViewById(R.id.pay_url);
+
+        dueDateSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePicker();
+            }
+        });
+
+        configureRepeatingSpinner();
+        checkIfEditing();
+
+    }
+
+    private void configureRepeatingSpinner() {
+        repeatingItem = new RepeatingItem(getString(R.string.repeating_item_monthly), RepeatingItem.CODE_MONTHLY);
+
+        final RepeatingSpinnerAdapter repeatingSpinnerAdapter = new RepeatingSpinnerAdapter(this, android.R.layout.simple_spinner_item);
+        repeatingSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        repeatingSpinner.setAdapter(repeatingSpinnerAdapter);
+        repeatingSpinner.setSelection(repeatingItem.toIndex());
+
+        repeatingSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                repeatingItem = repeatingSpinnerAdapter.getItem(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+    }
+
+    private void checkIfEditing() {
+        if (getIntent().getExtras() != null && getIntent().getBooleanExtra("edit", false)) {
+            loadBillForEditing(getIntent().getStringExtra("bill_uuid"));
+        }
+    }
+
+    private void showDatePicker() {
+        DatePickerDialog datePickerDialog = DatePickerDialog.newInstance(this, selectedDueDate.getYear(), selectedDueDate.getMonthOfYear() - 1, selectedDueDate.getDayOfMonth());
+        datePickerDialog.show(getFragmentManager(), "datePickerDialog");
+    }
+
+    private void loadBillForEditing(String billUuid) {
+        editing = true;
+        editBill = BillTrackerApplication.getRealm().where(Bill.class).contains("uuid", billUuid).findFirst();
+
+        name.setText(editBill.getName());
+        selectedDueDate = new DateTime(editBill.getDueDate());
+        setDueDateText(selectedDueDate);
+        repeatingItem = new RepeatingItem("", editBill.getRepeatingType());
+        repeatingSpinner.setSelection(repeatingItem.toIndex());
+        payUrl.setText(editBill.getPayUrl());
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        if (editing) {
+            getMenuInflater().inflate(R.menu.edit_bill_menu, menu);
+        } else {
+            getMenuInflater().inflate(R.menu.add_bill_menu, menu);
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_save) {
+            Realm realm = BillTrackerApplication.getRealm();
+            if (editing) {
+                realm.executeTransaction(new Realm.Transaction() {
+                    @Override
+                    public void execute(Realm realm) {
+                        editBill.setName(name.getText().toString());
+                        editBill.setRepeatingType(repeatingItem.code);
+                        editBill.setDueDate(selectedDueDate.toDate());
+                        editBill.setPayUrl(payUrl.getText().toString());
+                    }
+                });
+            } else {
+                final Bill b = new Bill(name.getText().toString(), "", repeatingItem.code, selectedDueDate.toDate(), payUrl.getText().toString());
+                realm.executeTransaction(new Realm.Transaction() {
+                    @Override
+                    public void execute(Realm realm) {
+                        realm.copyToRealm(b);
+                    }
+                });
+            }
+
+            Intent intent = new Intent();
+            intent.putExtra("delete", false);
+            setResult(RESULT_OK, intent);
+            finish();
+            return true;
+
+        } else if (id == R.id.delete_bill) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle(getString(R.string.confirm_delete));
+            builder.setMessage(getString(R.string.deletion_message, editBill.getName()));
+            builder.setPositiveButton(getString(R.string.yes), new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    deleteBill(editBill);
+                    dialog.dismiss();
+
+                    Intent intent = new Intent();
+                    intent.putExtra("delete", true);
+                    setResult(RESULT_OK, intent);
+                    finish();
+                }
+            });
+            builder.setNegativeButton(getString(R.string.cancel), null);
+
+            builder.show();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void deleteBill(final Bill bill) {
+        BillTrackerApplication.getRealm().executeTransaction(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+                bill.deleteFromRealm();
+            }
+        });
+    }
+
+
+    @Override
+    public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+        selectedDueDate = new DateTime(year, monthOfYear + 1, dayOfMonth, 0, 0);
+        setDueDateText(selectedDueDate);
+    }
+
+    private void setDueDateText(DateTime selectedDueDate) {
+        dueDateSelect.setText(selectedDueDate.toString("MMMM d"));
+    }
+}
